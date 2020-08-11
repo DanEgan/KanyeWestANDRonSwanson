8a5636d7fa66c56b58aa184583cceac821499377
DanEgan <you@example.com>
2020-08-11 12:04:44 -0400

    Second commit

---
+++ KanyeWestUI/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeWestUI
+{
+    class Program
+    {
+        static void KanyeQuote()
+        {
+            var client = new HttpClient();
+            var kanyeURL = "https://api.kanye.rest";
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+            Console.WriteLine($"Kanye says: \"{kanyeQuote}.\"");
+        }
+
+        static void SwansonQuote()
+        {
+            var client = new HttpClient();
+            var swansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var swansonResponse = client.GetStringAsync(swansonURL).Result;
+            var swansonQuote = JArray.Parse(swansonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            Console.WriteLine($"Ron responds: {swansonQuote}");
+            Console.WriteLine();
+        }
+
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                KanyeQuote();
+                SwansonQuote();
+            }
+
+        }
+    }
+}
